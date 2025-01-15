d776717582d2925bf7749374f6525775645ea2ed
@karlProbst <karlprobst@protonmail.com>
2025-01-15 16:29:50 -0300

    idk lol

--- fire_background.gd
+++ fire_background.gd
@@ -13,5 +13,4 @@ func _process(delta):
 func modulate_texture_offset(new_offset: Vector3,delta:float):
 
 	$GradientFireMask/FireRed.texture.noise.offset=offset
-	$GradientFireMask/FireRed.texture.color_ramp.set_offset(0, sin(offset.x/18)/2)
 	$BlobFireGradientMask/FireTextureRect.texture.noise.offset=offset
